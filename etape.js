cedccb00c9b41b61429b194bde81b671d9dd82d5
Louis Turpault <gameraccount@Pro-de-Louis.lan>
2021-05-11 20:25:06 -0400

    mon premier commit mon premier commit mon premier commit

---
+++ etape.js
@@ -0,0 +1,72 @@
+//3= t'es mort
+//2= tu progresse
+//1= tu pert 1 pv
+
+etapes = [
+  {
+    label: "vous êtes dans l'avion",
+    reponses: [
+      {
+        label: "vous vous endormez et pensez à votre poisson rouge",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 3,
+      },
+      {
+        label: "vous combattez les terroriste à main nue",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 3,
+      },
+      {
+        label:
+          "vous combattez les terroriste avec le fusil que vous avez apporté avec vous",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 2,
+      },
+      {
+        label: "vous sautez sans parachute",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+    ],
+  },
+  {
+    label: "vous êtes dans la ferme",
+    reponses: [
+      {
+        label: "vous sautez sans parachute",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+      {
+        label: "steve vous mange",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+      {
+        label: "REEEEEEEEEEEEEE",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+    ],
+  },
+  {
+    label: "vous êtes dans la ferme",
+    reponses: [
+      {
+        label: "vous sautez sans parachute",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+      {
+        label: "steve vous mange",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+      {
+        label: "REEEEEEEEEEEEEE",
+        video: "https://www.youtube.com/watch?v=ggp853CtBZw",
+        destination: 1,
+      },
+    ],
+  },
+];
