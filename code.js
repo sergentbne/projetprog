b3badb97a3b0fd024c75696400aa070ab0adeefb
sergentbne <louis.turpault@gmail.com>
2021-05-16 15:50:53 -0400

    questions

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
 function start(etape) {
+  const e = document.getElementById("questions");
+  e.innerHTML = "";
+  const etapedata = etapes[etape];
+  const titre = document.createElement("h1");
+  titre.innerText = etapedata.label;
+  e.appendChild(titre);
 
-    
+  // questions
+  const reponses = etapedata.reponses;
+  for (const reponse of reponses) {
+    const bouton = document.createElement("button");
+    bouton.innerText = reponse.label;
+    e.appendChild(bouton);
+  }
 }
