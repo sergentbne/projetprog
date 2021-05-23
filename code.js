e6f8a398949e16d9660e1c8ebfeacd1042083da6
Jocelyn Turpault <jocelyn.turpault@autodesk.com>
2021-05-23 18:37:38 -0400

    Les vies sont des coeurs

--- code.js
+++ code.js
@@ -3,15 +3,19 @@ var vies = 3;
 function start(etape) {
   const etapedata = etapes[etape];
 
-  if (etapedata.type=="mort"){
+  if (etapedata.type == "mort") {
     vies = vies - 1;
-    if (vies == 0){
+    if (vies == 0) {
       start(1);
       return;
-    } 
+    }
   }
- const vie = document.getElementById("vie")
-vie.innerText = "Il vous reste " + vies + " vies.";
+  if (etapedata.type == "vie") {
+    vies = vies + 1;
+  }
+
+  const vie = document.getElementById("vie");
+  vie.innerText = "❤️".repeat(vies);
 
   const e = document.getElementById("questions");
   e.innerHTML = "";
@@ -24,24 +28,23 @@ vie.innerText = "Il vous reste " + vies + " vies.";
   for (const reponse of reponses) {
     const bouton = document.createElement("button");
     bouton.innerText = reponse.label;
-    bouton.onclick = ()=>{
+    bouton.onclick = () => {
       start(reponse.destination);
       const v = document.getElementById("video");
-      var source = document.createElement('source');
+      var source = document.createElement("source");
 
-      source.setAttribute('src', reponse.video);
+      source.setAttribute("src", reponse.video);
 
       v.appendChild(source);
       v.play();
 
-      v.style.display = "block"
+      v.style.display = "block";
       v.requestFullscreen();
-      
     };
     e.appendChild(bouton);
   }
-const vid = document.getElementById("video")
-vid.onended = function() {
+  const vid = document.getElementById("video");
+  vid.onended = function () {
     vid.style.display = "none";
-};
+  };
 }
